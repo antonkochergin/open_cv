fe9b78380804893c2faacab3ef3a7fb02583f04e
antonkochergin <antonkochergin4@gmail.com>
2026-02-11 20:31:16 +0400

    Проверка

--- src/Project_face.py
+++ src/Project_face.py
@@ -14,11 +14,7 @@ result = faces.detectMultiScale(gray, scaleFactor=1.01, minNeighbors=5)
 # визуализвация
 for x, y, w, h in result:
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=2)
-
-
-
 cv2.imshow("Result", img)
 cv2.waitKey(0)
-
 
 # P.S.: Используется натренерованная нейронка
